30693c6b44d606c15f335bf29327577cc9c36608
Ben <Ben@localhost.localdomain>
2018-12-13 22:35:46 -0500

    Added most of the code for centered kernel target alignment. Only need to compute kernel values. Added libsvm library to support this effort.

--- src/main/java/edu/uri/cs/ga/scoring/CenteredKTAScorer.java
+++ src/main/java/edu/uri/cs/ga/scoring/CenteredKTAScorer.java
@@ -6,6 +6,8 @@ import edu.uri.cs.util.FileReaderUtils;
 
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by Ben on 12/11/18.
@@ -51,17 +53,124 @@ public class CenteredKTAScorer implements HypothesisScorerIF {
     }
 
     private double computeAccuracy() {
-        double totalExamples;
-        int totalCorrect;
-//        if (weighted) {
-//            totalExamples = 2.0 * positiveOutputParser.getTotalCount() * negativeOutputParser.getTotalCount();
-//            totalCorrect = positiveOutputParser.getCorrectCount() * negativeOutputParser.getTotalCount() +
-//                    negativeOutputParser.getCorrectCount() * positiveOutputParser.getTotalCount();
-//        } else {
-//            totalExamples = 1.0 * positiveOutputParser.getTotalCount() + negativeOutputParser.getTotalCount();
-//            totalCorrect = positiveOutputParser.getCorrectCount() + negativeOutputParser.getCorrectCount();
-//        }
-        return 0.0;
+        int size = outputParser.targets.keySet().size();
+        double[][] targetMatrix = new double[size][size];
+        double[][] kernelMatrix = new double[size][size];
+        computeMatrices(size, targetMatrix, kernelMatrix);
+
+        System.out.println("Matrices... before centering");
+        printMatrix(kernelMatrix); printMatrix(targetMatrix);
+
+        center_kernel_matrix(size, kernelMatrix);
+        center_kernel_matrix(size, targetMatrix);
+
+        System.out.println("Matrices... after centering");
+        printMatrix(kernelMatrix); printMatrix(targetMatrix);
+
+        double numer = compute_frobenius_product(kernelMatrix, targetMatrix);
+        double denom = compute_frobenius_norm(kernelMatrix, kernelMatrix);
+        double res = (denom != 0.0) ? numer / denom : 0.0;
+        return res;
+    }
+
+    private double compute_frobenius_norm(double[][] m, double[][] n) {
+        double res1 = compute_frobenius_product(m, m);
+        double res2 = compute_frobenius_product(n, n);
+        return Math.sqrt(res1 * res2);
+    }
+
+//    private double compute_frobenius_norm(double[][] m) {
+//        double res = compute_frobenius_product(m, m);
+//        return Math.sqrt(res);
+//    }
+
+    private double compute_frobenius_product(double[][] m, double[][] n) {
+        double res = 0.0;
+        double mult = 1.0;
+        // we cheat knowing that we are dealing with symmetric matrices
+        for (int i = 0; i < m.length; i++) {
+            for (int j = i; j < m[i].length; j++) {
+                mult = 2.0;
+                if (i == j) {
+                    mult = 1.0;
+                }
+                // this is the cheat - we just add the off diagonal terms twice
+                res += mult * m[i][j] * n[i][j];
+            }
+        }
+        return res;
+    }
+
+    private void printMatrix(double[][] table) {
+        for(int r=0; r<table.length; r++) {
+            for(int c=0; c<table[r].length; c++) {
+                System.out.print(table[r][c] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private void center_kernel_matrix(int size, double[][] m) {
+    /* compute normalizing values */
+        float[] meanrow = new float[size];
+        float correction = 0;
+        for (int i = 0; i < size; i++) {
+            meanrow[i] = 0;
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                meanrow[i] += m[i][j];
+                correction += m[i][j];
+                if (j != i) {
+                    meanrow[j] += m[i][j];
+                    correction += m[i][j];
+                }
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            meanrow[i] /= size;
+            System.out.println("meanrow[" + i + "] is: " + meanrow[i]);
+        }
+        correction /= (size * size);
+        System.out.println("correction is: " + correction);
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                // Note that meanrow[j] equals meancol[j] since the matrix is symmetric
+                // hence why it is not necessary to compute meancol;
+                // this is from Cortes paper, formula (1)
+                m[i][j] = m[i][j] - meanrow[i] - meanrow[j] + correction;
+                if (i != j) {
+                    m[j][i] = m[i][j];
+                }
+            }
+        }
+    }
+
+    private double[][] computeMatrices(int size, double[][] targetMatrix, double[][] kernelMatrix) {
+        double[] targetVec = new double[size];
+        double[][] featureVecMatrix = new double[size][size];
+        int k = 0;
+        for (String key : outputParser.targets.keySet()) {
+            targetVec[k] = outputParser.targets.get(key); k++;
+            featureVecMatrix[k] = outputParser.coveredClauses.get(key).
+                    stream().mapToDouble(Double::doubleValue).toArray();
+        }
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; i++) {
+                kernelMatrix[i][j] = computeKernelValue(featureVecMatrix[i], featureVecMatrix[j]);
+                targetMatrix[i][j] = targetVec[i] * targetVec[j];
+                if (i != j) {
+                    targetMatrix[j][i] = targetMatrix[i][j];
+                    kernelMatrix[j][i] = kernelMatrix[i][j];
+                }
+            }
+        }
+        return targetMatrix;
+    }
+
+    private double computeKernelValue(double[] v1, double[] v2) {
+        return 0;
     }
 
     private void validateParser(CommandLineOutputParser outputParser) {
@@ -90,8 +199,8 @@ public class CenteredKTAScorer implements HypothesisScorerIF {
 
     private class CommandLineOutputParser implements Consumer<String> {
 
-        HashMap<String, ArrayList<Double>> coveredClauses = new HashMap<>();
-        HashMap<String, Double> targets = new HashMap<>();
+        TreeMap<String, ArrayList<Double>> coveredClauses = new TreeMap<>();
+        TreeMap<String, Double> targets = new TreeMap<>();
         private static final String COVERED_STRING = "covered]";
         private static final String NOT_COVERED = "not covered";
         private boolean negate = false;
